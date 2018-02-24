9910afdb33c8df52067c07f47fae3878e68ad9b9
iysaleh <iysaleh@gmail.com>
2018-02-24 14:42:55 -0800

    attempt

--- server/app.js
+++ server/app.js
@@ -8,53 +8,24 @@ const Promise = require('promise');
 
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World again\n');
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World again\n');
 });
 
-/*function addEmployee (name,password) {
-  MongoClient.connect(url, function(err, db){
-    var employees = db.collection('employees');
-    var query = {name:name}
-    var empWithName = employees.find(query)
-	console.log(empWithName);
-	empWithName.then(function(results){
-		empWithName = results.toArray();
-		if (empWithName.length === 0){
-          employees.insert({name:name,password:password,meetings:[]});
-        }
-	}).catch(function(a_err){
-		console.log(a_err);
-	});
-	//console.log(empWithName);
-    /*employees.find(query).toArray(function(err, result){
-      if (err) {console.log(err);} 
-      //else if (a_result.length === 0) { employees.insert({name:name,password,password,meetings:[]}); }
-      //else {console.log("Employee already exists.");}
-	  
-	});
-	db.close(); 
-  });
-}*/
-
 function addEmployee (name,password) {
-  MongoClient.connect(url, function(err, db){
-    var employees = db.collection('employees');
-    var query = {name:name};
-    employees.find((query),(err,doc)=>{
-      if (err) {
-            employees.insert({name:name,password:password,meetings:[]});
-	} else {
-            console.log(doc);
-        }
-	db.close(); 
-    });
-}
-}
+	MongoClient.connect(url, function(err, db){
+		var employees = db.collection('employees');
+		var query = {name:name}
+		employees.find(query).toArray(function(err,employeesWithName){
+			if (employeesWithName.length === 0) {
+				employees.insert({name:name,password:password,meetings:[]});
+			}
+			db.close()
+		});
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-  addEmployee("test3","test3");
-  addEmployee("test3","test3");
+	console.log(`Server running at http://${hostname}:${port}/`);
+	addEmployee("test3","test3");
+	addEmployee("test3","test3");
 });

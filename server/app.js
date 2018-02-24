55036bd38bca539e53a7aae545fc9145d3a84ead
Michael Mar <michaelmar@MichaeletinaL13.shadowdomain>
2018-02-24 10:54:33 -0800

    Test employee

--- server/app.js
+++ server/app.js
@@ -8,6 +8,28 @@ const server = http.createServer((req, res) => {
   res.end('Hello World again\n');
 });
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/EventCalendar';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
+
+function addEmployee (name,password) {
+  MongoClient.connect(url, function(err, db) {
+    var employees= db.collection('employees');
+    employees.insert({name:name},{password:password},{meetings:[]});
+    db.close(); 
+  }
+}
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  addEmployee("test","test");
 });

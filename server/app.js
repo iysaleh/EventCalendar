eff6e5aa827f0fa80d6cf0e9401240c14c208351
iysaleh <iysaleh@gmail.com>
2018-02-24 13:29:01 -0800

    attempt

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ function addEmployee (name,password) {
     var query = {name:name}
     db.collection('employees').find(query).toArray(function(a_err, a_result){
       if (a_err) {console.log(a_err);} 
-      //else if (a_result.length === 0) { employees.insert({name:name,password,password,meetings:[]}); }
+      else if (a_result.length === 0) { employees.insert({name:name,password,password,meetings:[]}); }
       //else {console.log("Employee already exists.");}
 	});
 	db.close(); 

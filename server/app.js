ad52f00408df39095bc188d95f20ecd9ee82733b
iysaleh <iysaleh@gmail.com>
2018-02-24 13:40:59 -0800

    attempt

--- server/app.js
+++ server/app.js
@@ -15,7 +15,11 @@ function addEmployee (name,password) {
   MongoClient.connect(url, function(err, db){
     var employees = db.collection('employees');
     var query = {name:name}
-	employees.insert({name:name,password:password,meetings:[]});
+    var empWithName = employees.find(query).toArray()
+	console.log(empWithName);
+	if (empWithName.length === 0){
+      employees.insert({name:name,password:password,meetings:[]});
+    }
     /*employees.find(query).toArray(function(err, result){
       if (err) {console.log(err);} 
       //else if (a_result.length === 0) { employees.insert({name:name,password,password,meetings:[]}); }

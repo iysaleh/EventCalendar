1aedfd3261559408faa8824e5088775f5f1cbae9
Michael Mar <dkleener@yahoo.com>
2018-02-24 12:57:46 -0800

    Test Query 2

--- server/app.js
+++ server/app.js
@@ -17,12 +17,14 @@ function addEmployee (name,password) {
     var query = {name:name}
     db.collection('EventCalendar').find(query).toArray(function(a_err, a_result){
         if (a_err) employees.insert({name:name,password,password,meetings:[]});
-        else {throw "Employee already exits."};
-    db.close(); 
+        else {throw "Employee already exists."};
+        db.close(); 
 	});
   });
 }
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  addEmployee("test3","test3");
+  addEmployee("test3","test3");
 });

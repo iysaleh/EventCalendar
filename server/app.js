99f823af14cf3af6e97045836fa6ba96f30b052b
Michael Mar <michaelmar@MichaeletinaL13.shadowdomain>
2018-02-24 10:28:24 -0800

    Testing edit

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-const http = require('http');
-const hostname = '0.0.0.0';
+const http = require('http');// open port
+const hostname = '0.0.0.0'; //setting variables
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World again\n');
 });
 
 server.listen(port, hostname, () => {

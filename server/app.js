d5d0f044857ae6056abb8713f19453e149bdf9b9
Ibraheem Saleh <iysaleh@gmail.com>
2018-03-11 15:13:08 -0700

    Checkpoint add/delete employee

--- server/app.js
+++ server/app.js
@@ -16,7 +16,76 @@ const crypto = require('crypto');
 	res.end('Hello World again\n');
 });*/
 
-function addEmployee (username,password) {
+function addEmployee (requesterUser,requesterToken,username,password,fname,mname,lname,isAdmin) {
+	console.log(requesterUser);
+	verify = _verifyRequesterUserToken(requesterUser,requesterToken);
+	return new Promise( function(resolve) {
+		Promise.all([verify]).then(function(result){
+			if (result.length===0){
+				resolve("BAD_CREDENTIALS");
+			}
+			else{
+				MongoClient.connect(sUrl, function(err, db){
+					var employees = db.collection('employees');
+					var query = {username:username};
+					employees.find(query).toArray().then(function(result){
+						if (result.length === 0) {
+							employees.insert({username:username,password:password,isAdmin:isAdmin,startHour:"07:00:00",endHour:"19:00:00",fname:fname,mname:mname,lname:lname,meetings:[],notifications:[]});
+							resolve("USER_ADD_SUCCESS")
+							db.close()
+						}
+						else{
+							resolve("USERNAME_ALREADY_EXISTS");
+							db.close()
+						}
+					});
+				});
+			}
+		});
+	});
+}
+
+function deleteEmployee (requesterUser,requesterToken,username) {
+	console.log(requesterUser);
+	console.log(requesterToken);
+	verify = _verifyRequesterUserToken(requesterUser,requesterToken);
+	console.log("HELLOOOOO");
+	return new Promise( function(resolve) {
+		Promise.all([verify]).then(function(result){
+			console.log(result);
+			if (result.length===0){
+				console.log("BAD CREDS");
+				resolve("BAD_CREDENTIALS");
+			}
+			else{
+				console.log("VERIFIED! TIME TO DELETE.")
+				MongoClient.connect(sUrl, function(err, db){
+					var employees = db.collection('employees');
+					console.log('SENDING DELETE');
+					employees.remove({username:username});
+					resolve("USER_DELETE_SUCCESS");
+					db.close();
+				});
+			}
+		});
+	});
+}
+
+function _verifyRequesterUserToken(requesterUser,requesterToken){
+	return new Promise( function(resolve) {
+		requesterToken = parseInt(requesterToken);
+		MongoClient.connect(sUrl, function(err, db){
+			db.collection('employees').find({username:requesterUser,sessionToken:requesterToken}).map(function(item){
+				return {username:item.username,isAdmin:item.isAdmin};
+			}).toArray().then(function(result){
+				resolve(result[0]);
+				db.close();
+			});
+		});
+	});
+}
+
+function addEmployeeOld (username,password) {
 	MongoClient.connect(sUrl, function(err, db){
 		var employees = db.collection('employees');
 		var query = {username:username}
@@ -29,14 +98,22 @@ function addEmployee (username,password) {
 	});
 }
 
-function getEmployeeList() {
+function getEmployeeList(requesterUser,requesterToken) {
+	verify = _verifyRequesterUserToken(requesterUser,requesterToken);
 	return new Promise( function(resolve) {
-		MongoClient.connect(sUrl, function(err, db){
-			db.collection('employees').find().map(function(item){
-				return item.username;
-			}).toArray().then(function(result){
-				resolve(result);
-			});
+		Promise.all([verify]).then(function(result){
+			if (result.length===0){
+				resolve("BAD_CREDENTIALS");
+			}
+			else{
+				MongoClient.connect(sUrl, function(err, db){
+					db.collection('employees').find().map(function(item){
+						return item.username;
+					}).toArray().then(function(result){
+						resolve(result);
+					});
+				});				
+			}
 		});
 	});
 }
@@ -45,7 +122,7 @@ function login (username,password) {
 	return new Promise( function(resolve) {
 		MongoClient.connect(sUrl, function(err, db){
 			db.collection('employees').find({username:username, password:password}).map(function(item){
-				return {username:item.username, meetings:item.meetings, notifications:item.notifications, sessionToken:item.sessionToken,fName:item.fName};
+				return {username:item.username, meetings:item.meetings, notifications:item.notifications, sessionToken:item.sessionToken,fName:item.fName, isAdmin:item.isAdmin};
 			}).toArray().then(function(result){
 				if (result.length===1){
 					//Generate login Session token if not exist
@@ -125,6 +202,7 @@ var server = connect()
 			var query;
 			var url_parts = url.parse(req.url, true);
 			query = url_parts.query;
+			console.log(query);
 
 			if (req.method === 'GET') {
 				switch (url_parts.pathname) {
@@ -143,19 +221,37 @@ var server = connect()
 					console.log(query);
 					break;
 					case '/getEmployeeList':
-					getEmployeeList().then(function(result){
+					getEmployeeList(query.requesterUser,query.requesterToken).then(function(result){
 						res.body = JSON.stringify(result);
+						res.setHeader('Content-Type', 'application/json');
+						res.end(res.body);
+					});
+					break;
+				}
+			} else if (req.method === 'POST') {
+				switch (url_parts.pathname) {
+					case '/addEmployee':
+					addEmployeeResult = addEmployee(query.requesterUser,query.requesterToken,query.user,query.pass,query.fname,query.mname,query.lname,query.isAdmin)
+					console.log("WHY ARE YOU STUCK");
+					Promise.all([addEmployeeResult]).then(function(result){
+						console.log("IN THE PROMISE");
+						res.body = result[0];
+						console.log(res.body);
 						res.setHeader('Content-Type', 'text/plain');
 						res.end(res.body);
 					});
 					break;
-
-				}
-			} else if (req.method === 'POST') {
-				switch (url_parts.pathname) {
-					case '/somepath2':
-					// do something
-					res.end();
+					case '/deleteEmployee':
+					console.log(query);
+					deleteEmployeeResult = deleteEmployee(query.requesterUser,query.requesterToken,query.username)
+					
+					Promise.all([deleteEmployeeResult]).then(function(result){
+						console.log("RESULT: RESULT");
+						res.body=result[0];
+						console.log(res.body);
+						res.setHeader('Content-Type', 'text/plain');
+						res.end(res.body);
+					});
 					break;
 				}
 			} else {
